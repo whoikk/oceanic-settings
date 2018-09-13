467d1abdac8af0b9783b366c0ac64cd5a272b97a
Ashish Mahla <ashishmahla96@gmail.com>
2018-09-14 01:00:30 +0530

    redefined whole library code

--- app/src/main/java/in/scarecrow/oceanicsettingsdemo/MainActivity.java
+++ app/src/main/java/in/scarecrow/oceanicsettingsdemo/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+
+        // TODO: 14-09-2018 create a demo example app
     }
 }
